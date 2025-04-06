d2328e89c60cef14dbb5e3d9c59947db6d89ad4b
Sergho <sergche04@gmail.com>
2025-04-06 15:45:56 +0300

    feat: get custom candles

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.Robot import Robot
 
 if __name__ == "__main__":
-    
-    robot = Robot()
-    robot.test()
+    robot = Robot(
+        clientCode="10134",
+        accountId="NL0011100043",
+        classCode="QJSIM",
+        tickerCode="SBER"
+    )
+    print(robot.get_candles(count=3))
+    robot.close_connection()
--- src/Robot.py
+++ src/Robot.py
@@ -1,12 +1,20 @@
 from QUIK.QuikPy import QuikPy
 
 class Robot:
-    def __init__(self, clientCode: str, accountId: str, classCode: str, ticketCode: str):
+    def __init__(self, clientCode: str, accountId: str, classCode: str, tickerCode: str):
         self._provider = QuikPy()
         self._clientCode = clientCode
-        self.account = accountId
-        self.classCode = classCode
-        self.ticketCode = ticketCode
+        self._account = accountId
+        self._classCode = classCode
+        self._tickerCode = tickerCode
         
-    def __del__(self):
+    def close_connection(self):
         self._provider.close_connection_and_thread()
+        
+    def get_candles(self, interval: int = 1, count: int = 5):
+        return self._provider.get_candles_from_data_source(
+            class_code=self._classCode,
+            sec_code=self._tickerCode,
+            interval=interval,
+            count=count
+        )
