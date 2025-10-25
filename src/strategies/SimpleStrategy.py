24cf4486f195bb6f30f2e079e54615d8b70ecf78
Lms-media <maksimap2013@gmail.com>
2025-10-25 13:53:05 +0300

    edit SimpleStrategy

--- src/strategies/SimpleStrategy.py
+++ src/strategies/SimpleStrategy.py
@@ -1,6 +1,7 @@
 from src.strategies.Strategy import Strategy
 from src.data.Candle import Candle
-import random
+import pandas as pd
+from dataclasses import asdict
 
 
 class SimpleStrategy(Strategy):
@@ -8,27 +9,11 @@ class SimpleStrategy(Strategy):
         self.chunkSize = chunkSize
 
     def predict(self, chunk: list[Candle]) -> float:
-        reversedCandles = chunk[::-1]
-        stableCount = 1
-        diff = reversedCandles[0].close - reversedCandles[1].close
-        previous = reversedCandles[1]
-        current = reversedCandles[2]
-        i = 3
-        while diff * (previous.close - current.close) > 0:
-            stableCount += 1
-            previous = current
-            current = reversedCandles[i]
-            i += 1
-            if i == 5:
-                break
-        close = 0
-        for j in range(stableCount):
-            close += reversedCandles[j].close - reversedCandles[j + 1].close
-
-        del2 = close / stableCount
-        close = reversedCandles[0].close
-        sign = 1 if (j // stableCount) % 2 == 0 else -1
-        close += del2 * sign
-
-        return random.choice([-1000, 0, 1000])
-        # return 1 if chunk[-1].close > close else -1
+        data = [asdict(candle) for candle in chunk]
+        df = pd.DataFrame(data)
+        
+        df['close_ma'] = df['close'].rolling(window=self.chunkSize).mean()
+        
+        if len(df) > 1 and df['close'].iloc[-1] > df['close'].iloc[-2]:
+            return abs(df['close'].iloc[-1] - df['close'].iloc[-2])
+        return -abs(df['close'].iloc[-1] - df['close'].iloc[-2])
