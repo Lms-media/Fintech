1892477099a8c12500bd28957881657f586ac5c7
Sergho <sergche04@gmail.com>
2026-01-29 20:28:23 +0300

    add primitives

---
+++ src/primitives/enums/ActionStatus.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class ActionStatus(Enum):
+    Waiting = 1
+    Executing = 2
+    Finished = 3
---
+++ src/primitives/interfaces/IValueObject.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from typing import Any, TypeVar, Generic
+
+T = TypeVar('T')
+
+class IValueObject(ABC, Generic[T]):
+
+    @abstractmethod
+    def equals(self, other: T) -> bool:
+        pass
+
+    @abstractmethod
+    def copy(self) -> T:
+        pass
